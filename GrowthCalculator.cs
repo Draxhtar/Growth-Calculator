fd1d184d4893b3f21eeef58c3a6e22e33349c7b5
Draxhtar <60029920+Draxhtar@users.noreply.github.com>
2021-11-20 18:43:22 +0300

    Formatting - Update GrowthCalculator.cs
    +Changed some namings.
    +Edited some comments.
    +Minor formatting changes

--- GrowthCalculator.cs
+++ GrowthCalculator.cs
@@ -4,26 +4,29 @@ public class GrowthCalculator
 {
     static public void Main ()
     {
-        //NUMBERS (changeable)
-        int firstDayNum = 1; //At what point you are at the first day
+        //VARIABLES (Change these to fit your needs)
+        int firstDayAmount = 1; //At what point you are at the first day
         int growthPercentage = 1; //Growth percentage on daily basis. Example: read %1 more books than the last day, everyday
         int forHowManyDays = 365; //How many days do you want this calculation to take into account?
         
         //IGNORE
-        double currentNum = firstDayNum;
+        double currentAmount = firstDayAmount;
         double grandTotal = currentNum;
         
         //ALGORITHM        
         for(int i = 0; i < forHowManyDays; i++)
         {
-            currentNum += (currentNum * growthPercentage)/100;
-            Console.WriteLine((i + 1) + ". Day = " + currentNum);
-            grandTotal += currentNum;
-        } 
+            currentAmount += (currentAmount * growthPercentage)/100;
+            grandTotal += currentAmount; //Keep track of total amount
+            
+            //OUTPUTTING
+            Console.WriteLine((i + 1) + ". Day = " + currentAmount);
+        }
         
-        //OUTPUTTING
+        //OUTPUTTING FINAL RESULTS
         Console.WriteLine("Grand Total Pages = " + grandTotal);
-        Console.WriteLine(grandTotal / 365); 
-
+        
+        double dailyAverage = grandTotal / 365;
+        Console.WriteLine(dailyAverage);
     }
 }
